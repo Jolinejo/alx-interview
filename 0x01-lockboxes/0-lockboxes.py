9faedc74f9403de337dce45f1a534ba682fc27a9
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-14 23:09:12 +0200

    initial guess

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    checks if all boxes can be opened
+    """
+    queue = []
+    uniq = set()
+    uniq.add(0)
+    for i in boxes[0]:
+        if i not in uniq:
+            uniq.add(i)
+            queue.append(i)
+        
+    while len(queue):
+        for i in boxes[queue[0]]:
+            if i not in uniq:
+                uniq.add(i)
+                queue.append(i)
+        queue.pop(0)
+    if len(uniq) == len(boxes):
+        return True
+    return False
