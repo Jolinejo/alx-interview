c90693bd7f62493a1fcfc897e9fdec7b87eecbae
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-14 23:12:07 +0200

    2nd guess

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,6 +3,7 @@
 lockboxes
 """
 
+
 def canUnlockAll(boxes):
     """
     checks if all boxes can be opened
@@ -11,13 +12,13 @@ def canUnlockAll(boxes):
     uniq = set()
     uniq.add(0)
     for i in boxes[0]:
-        if i not in uniq:
+        if i not in uniq and i < len(boxes):
             uniq.add(i)
             queue.append(i)
-        
+
     while len(queue):
         for i in boxes[queue[0]]:
-            if i not in uniq:
+            if i not in uniq and i < len(boxes):
                 uniq.add(i)
                 queue.append(i)
         queue.pop(0)
