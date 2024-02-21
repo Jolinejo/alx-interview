dd76b598366ec0f607c92c7bea83119295cf4668
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-22 00:54:35 +0200

    task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+min ops
+"""
+
+import math as mt
+
+maxn = 100001
+
+primes = [i for i in range(maxn)]
+
+
+def sieve():
+
+    for i in range(4, maxn, 2):
+        primes[i] = 2
+
+    for i in range(3, mt.ceil(mt.sqrt(maxn))):
+        if primes[i] == i:
+            for j in range(i * i, maxn, i):
+                if primes[j] == j:
+                    primes[j] = i
+
+
+sieve()
+
+
+def get_factors(n):
+    ret = []
+    while n != 1:
+        ret.append(primes[n])
+        print(primes[n])
+        n = n // primes[n]
+
+    return ret
+
+
+def minOperations(n):
+    listi = get_factors(n)
+    print(listi)
+    sum = 0
+    for i in listi:
+        sum += i
+    return sum
