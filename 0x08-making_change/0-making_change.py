5108e6b26df8bafc668e9351653776283cb5c91e
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-04-04 20:40:51 +0200

    task

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,8 +15,9 @@ def makeChange(coins, total):
 
     dp[0] = 0
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+               dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
